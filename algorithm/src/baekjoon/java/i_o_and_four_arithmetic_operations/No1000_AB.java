02986b42203edce7d0f2355ff06f161f452adc73
CHUNYOUNGSANG <cys07028@naver.com>
2024-03-21 20:53:19 +0900

    [백준] A+B

--- algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No1000_AB.java
+++ algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No1000_AB.java
@@ -1,7 +1,26 @@
 package baekjoon.java.i_o_and_four_arithmetic_operations;
 
-public class No1000_AB {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class No1000_AB {    // 백준 제출할 때는 Main 으로 변경해야 함
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();   // 정수 A를 입력받음
+        int B = sc.nextInt();   // 정수 B를 입력받음
+
+        System.out.println(A + B);  // A와 B를 더한 값을 출력
+
+        sc.close(); // Scanner를 닫음
     }
 }
+
+/*
+ 문제 : 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오
+
+ 입력 : 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+ 출력 : 첫째 줄에 A+B를 출력한다.
+
+ 예제 입력 1 : 1 2
+ 에제 출력 1 : 3
+ */
