2cc4d1eb1338eda6a4762d2c768ff83dd6f9cabd
CHUNYOUNGSANG <cys07028@naver.com>
2024-03-21 20:45:11 +0900

    [백준] Hello World

---
+++ algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No1000_AB.java
@@ -0,0 +1,7 @@
+package baekjoon.java.i_o_and_four_arithmetic_operations;
+
+public class No1000_AB {
+    public static void main(String[] args) {
+
+    }
+}
--- algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No2557_HelloWorld.java
+++ algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No2557_HelloWorld.java
@@ -6,3 +6,14 @@ public class No2557_HelloWorld {    // 제출할 때는 Main 으로 변경해야
 
     }
 }
+
+/*
+ 문제 : Hello World!를 출력하시오
+
+ 입력 : 없음
+
+ 출력 : Hello World!를 출력하시오
+
+ 예제 입력 1 :-
+ 예제 출력 1 : Hello World!
+ */
