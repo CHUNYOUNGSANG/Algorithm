0c569fba976ce9dbc41de8bc9ef87bb242bc3529
CHUNYOUNGSANG <cys07028@naver.com>
2024-03-25 11:34:25 +0900

    [백준] 고양이

---
+++ algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No10171_Cat.java
@@ -0,0 +1,26 @@
+package baekjoon.java.i_o_and_four_arithmetic_operations;
+
+public class No10171_Cat {  // 백준 제출할 때는 Main 으로 변경해야 함
+    public static void main(String[] args) {
+
+        System.out.println("\\    /\\");     // 역슬래시(\)를 출력하기 위해 이스케이프 문자 \\를 사용해서 출력
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
+
+/*
+ 문제 : 아래 예제와 같이 고양이를 출력하시오.
+
+ 입력 : 없음.
+
+ 출력 : 고양이를 출력한다
+
+ 예제 입력 1 =
+ 예제 출력 1 =  \    /\
+                )  ( ')
+                (  /  )
+                 \(__)|
+ */
