a0c37a9ce76eab44d89a6e04b1b39611fe857f9c
CHUNYOUNGSANG <cys07028@naver.com>
2024-03-25 21:03:19 +0900

    [백준] 개

---
+++ algorithm/src/baekjoon/java/i_o_and_four_arithmetic_operations/No10172_Dog.java
@@ -0,0 +1,29 @@
+package baekjoon.java.i_o_and_four_arithmetic_operations;
+
+public class No10172_Dog {
+    public static void main(String[] args) {
+        // 이스케이프 문자(\, ', ")는 단독으로 사용할 경우 출력할 수 없다.
+        // 따라서 역슬래시(\)와 같은 문자와 결합하여 사용하게 된다.
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+
+/*
+ 문제 : 아래 예제와 같이 개를 출력하시오.
+
+ 입력 : 없음.
+
+ 출력 : 개를 출력한다.
+
+ 예제 입력 = -
+ 예제 출력 = |\_/|
+           |q p|   /}
+           ( 0 )"""\
+           |"^"`    |
+           ||_/=\\__|
+ */
