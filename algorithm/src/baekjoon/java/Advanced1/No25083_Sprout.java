e2cc17858a80bdb689682d85398e2413e9376778
CHUNYOUNGSANG <cys07028@naver.com>
2024-04-22 12:47:14 +0900

    [백준] 새싹

---
+++ algorithm/src/baekjoon/java/Advanced1/No25083_Sprout.java
@@ -0,0 +1,28 @@
+package baekjoon.java.Advanced1;
+
+public class No25083_Sprout {   // 백준 제출할 때는 Main 으로 변경해야 함.
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
+
+/*
+ 문제 : 아래 예제와 같이 새싹을 출력하시오.
+
+ 입력 : 입력은 없다.
+
+ 출력 : 새싹을 출력한다.
+
+ 예제 입력 1 =
+ 예제 출력 1 =          ,r'"7
+             r`-_   ,'  ,/
+             \. ". L_r'
+             `~\/
+                 |
+                 |
+ */
